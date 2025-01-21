c6bb48a155b67beb513630b302974e43576ce432
Nish344 <148205163+Nish344@users.noreply.github.com>
2025-01-21 07:51:58 +0530

    Create linkedStack.c

---
+++ linkedStack.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *top = NULL;
+
+void push(int);
+void pop();
+void display();
+
+void main() {
+    int choice, value;
+
+    printf("\n Stack using Linked List\n");
+
+    while (1) {
+        printf("\n****** MENU ******\n");
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the value to be inserted: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                exit(0);
+                break;
+
+            default:
+                printf("\nWrong selection!!! Please try again!!!\n");
+        }
+    }
+}
+
+void push(int value) {
+    struct Node *newNode;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+
+    if (top == NULL) {
+        newNode->next = NULL;
+    } else {
+        newNode->next = top;
+    }
+
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("\nStack is Empty!!!\n");
+    } else {
+        struct Node *temp = top;
+        printf("\nDeleted element: %d\n", temp->data);
+        top = temp->next;
+        free(temp);
+    }
+}
+
+void display() {
+    if (top == NULL) {
+        printf("\nStack is Empty!!!\n");
+    } else {
+        struct Node *temp = top;
+        while (temp != NULL) {
+            printf("%d--->", temp->data);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
